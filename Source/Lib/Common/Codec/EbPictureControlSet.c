8be76c10fcb24f20a8125e9879f394a2c1fba311
Xu Guangxin <guangxin.xu@intel.com>
2019-06-19 23:24:17 +0800

    hashtable: use EB_MALLOC and EB_FREE

--- Source/Lib/Common/Codec/EbPictureControlSet.c
+++ Source/Lib/Common/Codec/EbPictureControlSet.c
@@ -116,6 +116,7 @@ void picture_control_set_dctor(EbPtr p)
 {
     PictureControlSet* obj = (PictureControlSet*)p;
     uint8_t depth;
+    av1_hash_table_destroy(&obj->hash_table);
     EB_DELETE(obj->enc_dec_segment_ctrl);
     EB_DELETE(obj->ep_intra_luma_mode_neighbor_array);
     EB_DELETE(obj->ep_intra_chroma_mode_neighbor_array);
