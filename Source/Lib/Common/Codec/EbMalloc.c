22a74fd533ef474420d25be00891fbb1e84733e0
Xu Guangxin <guangxin.xu@intel.com>
2019-07-01 02:01:05 -0400

    memory profile: reduce g_mem_entry size from 32M to 4M
    this will save 1G memory for hash bucket in debug mode

--- Source/Lib/Common/Codec/EbMalloc.c
+++ Source/Lib/Common/Codec/EbMalloc.c
@@ -71,7 +71,7 @@ typedef struct MemoryEntry{
 } MemoryEntry;
 
 //+1 to get a better hash result
-#define MEM_ENTRY_SIZE (32 * 1024 * 1024 + 1)
+#define MEM_ENTRY_SIZE (4 * 1024 * 1024 + 1)
 
 MemoryEntry g_mem_entry[MEM_ENTRY_SIZE];
 
