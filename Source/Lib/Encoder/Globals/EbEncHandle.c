d244db9d5c8920343b677cb9a40ce84c84673c14
Colin Chan <colinchan02@gmail.com>
2020-04-15 10:46:20 -0700

    Fix tab style check issue

--- Source/Lib/Encoder/Globals/EbEncHandle.c
+++ Source/Lib/Encoder/Globals/EbEncHandle.c
@@ -1966,7 +1966,7 @@ void set_param_based_on_input(SequenceControlSet *scs_ptr)
         scs_ptr->static_config.super_block_size = (scs_ptr->static_config.enc_mode <= ENC_M3) ? 128 : 64;
 #endif
 #if FIX_RC_SB_SIZE
-	scs_ptr->static_config.super_block_size = (scs_ptr->static_config.rate_control_mode > 0) ? 64 : scs_ptr->static_config.super_block_size;
+    scs_ptr->static_config.super_block_size = (scs_ptr->static_config.rate_control_mode > 0) ? 64 : scs_ptr->static_config.super_block_size;
 #else
     scs_ptr->static_config.super_block_size = (scs_ptr->static_config.rate_control_mode > 1) ? 64 : scs_ptr->static_config.super_block_size;
 #endif
